5b26851c2d67411fcd813e4e2de6d86977521fff
Ankush <ankushsharma9685@gmail.com>
2023-10-22 12:24:11 +0530

    added signupform and js functionality of signup form

---
+++ frontend/App/components/signup/js/signup.js
@@ -0,0 +1,29 @@
+const signupform = document.getElementById("signupform");
+signupform.addEventListener("submit", registerUser);
+
+function registerUser(e) {
+    e.preventDefault();
+    const formdata = new FormData(e.target);
+    let userdata = {}
+
+    for (let [name, value] of formdata) {
+        userdata[name] = value;
+    }
+
+    //password confirmation
+    if (userdata.password != userdata.confirmpassword) {
+        document.getElementById('passwordmessage').removeAttribute('hidden');
+        return;
+    } else {
+        document.getElementById('passwordmessage').setAttribute('hidden', '');
+    }
+    //data except confirmpassword
+    let { confirmpassword, ...updatedUserData } = userdata;
+    axios.post("http://localhost:3000/auth/signup", updatedUserData).then((result) => {
+        if (result.status === 200) {
+            window.location.href = "../../login/html/login.html";
+        }
+    }).catch(err => {
+        console.log(err);
+    })
+}
