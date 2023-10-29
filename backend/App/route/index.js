4498cbe2ef37e2f67019455a9ee77a0f9e7fd0e7
Ankush <ankushsharma9685@gmail.com>
2023-10-29 12:57:51 +0530

    if no path exist then send to absolute path of login.html added in inregrating frontend

--- backend/App/route/index.js
+++ backend/App/route/index.js
@@ -7,6 +7,7 @@ const purchaseRoutes = require("./purchase-routes.js");
 const premiumRoutes = require("./premium-routes.js");
 const incomeRoutes = require("./income-route.js");
 const path = require("path");
+const fs = require("fs")
 
 router.use("/auth", authRoutes);
 router.use("/expense", Authentication, expenseRoutes);
@@ -15,10 +16,21 @@ router.use("/purchase", Authentication, purchaseRoutes);
 router.use("/premium", Authentication, premiumRoutes);
 
 //if no route found
-router.use((req, res, next) => {
-    console.log(req.url);
-    res.sendFile(path.join(__dirname,`../public/components/${req.url}`))
-    // res.status(404).send("no routes found");
-})
+router.use('/', (req, res, next) => {
+    //get file path
+    let filePath = path.join(__dirname, `../../public/components/${req.url}`);
+
+    //check if the path exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        //if not or path is root, redirect to login page
+        if (err || req.url === "/") {
+            //adding absolute path so it doesnt add relative path in other request
+            res.redirect(`${process.env.DOMAIN}/login/html/login.html`);
+        } else {
+            res.sendFile(filePath);
+        }
+    });
+});
+
 
 module.exports = router;
