116745491fa6319be719fc6f39f2befdca6b71b3
Ankush <ankushsharma9685@gmail.com>
2023-12-09 23:15:15 +0530

    remvoed default from userId

---
+++ backend/App/models/order.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    paymentid: {
+        type: String
+    },
+    orderid: {
+        type: String
+    },
+    status: {
+        type: String
+    },
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',                   //referenec to user
+    }
+})
+
+
+module.exports = mongoose.model("Order", orderSchema);
