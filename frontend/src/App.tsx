d0809c6a718b5f09c16dfb783575f541f4b8e25c
Ankush <ankushsharma9685@gmail.com>
2024-01-22 16:11:55 +0530

    updated app.js to handel language selection and theme change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import "./App.css";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import i18next from "i18next";
-import { Select, MenuItem, Box } from "@mui/material";
+import { Select, MenuItem, Box, ThemeProvider, Paper } from "@mui/material";
 import { I18nContext, setLanguage } from "./contexts/Translate/translate";
 import AppRoutes from "./routes";
+import darkTheme from "./assets/theme/dark";
+import lightTheme from "./assets/theme/light";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 const App = () => {
   const [lang, setLang] = useState("en");
+  const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(false);
 
   const handleLangChange = (event: any) => {
     const newLang = event.target.value;
@@ -14,34 +20,38 @@ const App = () => {
     setLanguage(newLang);
   };
 
+  const changeTheme = () => setIsDarkModeEnabled(!isDarkModeEnabled);
+  console.log(isDarkModeEnabled);
+
   return (
-    <BrowserRouter>
-      <I18nContext.Provider value={{ lang, t: i18next.t.bind(i18next) }}>
-        <AppRoutes />
-        <Box
-          sx={{
-            position: "fixed",
-            top: "80px",
-            right: { md: "60px", xs: "20px" },
-          }}
-        >
-          <Select
-            value={lang}
-            onChange={handleLangChange}
+    <ThemeProvider theme={isDarkModeEnabled ? darkTheme : lightTheme}>
+      <Paper>
+        <I18nContext.Provider value={{ lang, t: i18next.t.bind(i18next) }}>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+          <Box
+            className="selectlanguageBox"
             sx={{
-              width: "100px",
-              ".css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
-                display: "none",
-              },
+              right: { md: "60px", xs: "20px" },
             }}
           >
-            <MenuItem value="hi">भाषा</MenuItem>
-            <MenuItem value="tam">மொழி</MenuItem>
-            <MenuItem value="en">LANG</MenuItem>
-          </Select>
-        </Box>
-      </I18nContext.Provider>
-    </BrowserRouter>
+            <Select value={lang} onChange={handleLangChange}>
+              <MenuItem value="hi">भाषा</MenuItem>
+              <MenuItem value="tam">மொழி</MenuItem>
+              <MenuItem value="en">LANG</MenuItem>
+            </Select>
+            <Box className="themeMode">
+              {isDarkModeEnabled ? (
+                <LightModeIcon onClick={changeTheme} />
+              ) : (
+                <DarkModeIcon onClick={changeTheme} />
+              )}
+            </Box>
+          </Box>
+        </I18nContext.Provider>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
